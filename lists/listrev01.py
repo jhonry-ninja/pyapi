29a5e8b1888920bc1da9fc565b7aeaad36772cd3
JC <jcopinaldo@gmail.com>
2021-04-12 16:24:53 -0700

    Playing with lists

---
+++ lists/listrev01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""JC | Learning About Lists"""
+     
+def main():
+    ## create an empty list
+    myemptylist = []
+
+    ## add to our list with a list method
+    ## The extend method will add every item to the list
+    myemptylist.extend('192.168.102.55')
+
+    ## display our list
+    print(myemptylist)
+
+if __name__ == "__main__":
+    main()
